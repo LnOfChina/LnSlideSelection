19c5ff204d04404e245a4bb177481bd2545de3d8
bolang <1015289138@qq.com>
2018-09-05 18:36:37 +0800

    项目创建

---
+++ LnSlideSelection/LnSlideSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LnSlideSelection
+//
+//  Created by talking on 2018/9/5.
+//  Copyright © 2018年 李宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LnSlideSelection/LnSlideSelectionTests/LnSlideSelectionTests.m
@@ -0,0 +1,39 @@
+//
+//  LnSlideSelectionTests.m
+//  LnSlideSelectionTests
+//
+//  Created by talking on 2018/9/5.
+//  Copyright © 2018年 李宁. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LnSlideSelectionTests : XCTestCase
+
+@end
+
+@implementation LnSlideSelectionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
